b92d66beb91584cc164f8153d475be2215a593c8
Chakrapani Upadhyaya <source.lastbenchcoder@gmail.com>
2022-08-20 18:04:48 +0530

    images changes added

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const cors =require("cors")
+const cors = require("cors")
 require("dotenv").config()
 const AdministratorRoute = require("./routes/administrator")
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    },
+})
+
+const upload = multer({ storage: storage })
 
 const sampleData = {
     "appName": "cwc-user-manager-api",
@@ -14,6 +26,11 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/images', express.static('images'));
+
+app.post('/api/upload', upload.single('file'), function (req, res) {
+    res.json(req.file.filename)
+})
 
 app.get("/api/status", (req, res) => {
     res.send(sampleData)
@@ -27,7 +44,7 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
         console.log("Error Connecting in Mongo Database")
     })
 
-app.use("/api/administrator",AdministratorRoute)
+app.use("/api/administrator", AdministratorRoute)
 
 const PORT = process.env.PORT || 9999;
 
